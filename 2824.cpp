695907517a61b49c5811bdc468a42f4126dfcedf
umsuchi <ummulmomeninsuchi@gmail.com>
2025-06-10 12:06:31 +0600

    Count Pairs Whose Sum is Less Than Target

---
+++ 2824.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int count = 0 , n = nums.size();
+        for(int i = 0 ; i<n ; i++){
+            for(int j = i + 1; j<n ; j++){
+                if(nums[i] + nums[j] < target)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
