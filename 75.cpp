dea87e495bee954bae8f5154aa4071cffc817404
umsuchi <ummulmomeninsuchi@gmail.com>
2025-09-08 19:11:12 +0600

    Dutch National Flag Algorithm

---
+++ 75.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, mid = 0, h = n - 1;
+
+        while(mid <= h){
+            if(nums[mid] == 0) swap(nums[l], nums[mid]), mid++, l++;
+            else if(nums[mid] == 1) mid++;
+            else swap(nums[h], nums[mid]), h--;
+        }
+    }
+}; // 3 pointers ,,, Dutch National Flag Algorithm
