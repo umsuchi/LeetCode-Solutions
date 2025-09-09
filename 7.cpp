1664da8b55f01ad33de7f29a04f6b84027fd7894
umsuchi <ummulmomeninsuchi@gmail.com>
2025-09-10 00:08:03 +0600

    REVER

---
+++ 7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while(x != 0){
+            int n = x % 10;
+            if(rev > INT_MAX / 10 && rev < INT_MIN / 10) 
+                return 0;
+
+            rev *= 10;
+            rev += n;
+            x /= 10;
+        }
+
+        return rev;
+    }
+};
