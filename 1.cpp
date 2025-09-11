3b885395e4478cf1434ab3faecc0e63a59d18ca6
umsuchi <ummulmomeninsuchi@gmail.com>
2025-09-12 00:30:36 +0600

    Two Sum optimized

--- 1.cpp
+++ 1.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int tar) {
+        unordered_map<int,int> m;
         vector<int> ans;
         for(int i = 0 ; i<nums.size() ; i++){
-           for(int j = (i+1) ; j< nums.size() ; j++){
-            if((nums[i] + nums[j]) == target)
-            {
-                ans.push_back(i);
-                ans.push_back(j);
-            }
+           int f = nums[i], s = tar - f;
+
+           if(m.find(s) != m.end()){
+            ans.push_back(i);
+            ans.push_back(m[s]);
+            break;
            }
+           m[f] = i;
         }
         return ans;
 
